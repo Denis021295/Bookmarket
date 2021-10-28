06ddc9e8132be063ebbff5f90aae6fab643d66ea
Denis Bakitko <denisbakitko@bitbucket.org>
2021-10-28 17:03:48 +0300

    changed footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,13 +3,23 @@
     <p class="float-end mb-1">
       <a href="#">Back to top</a>
     </p>
-    <p class="mb-1">BookMarket&copy; My first Laravel Project =)</p>
+    <p class="h5"> 
+      <i class="bi bi-github"></i> 
+      <a 
+        href="https://github.com/Denis021295/Bookmarket"
+        target="_blank"
+        class="text-decoration-none text-dark"
+      >
+        Bookmarket
+      </a>
+      | My first Laravel project
+    </p> 
   </div>
 </footer>
 
 
     <script 
-    	src="https://getbootstrap.com/docs/5.1/dist/js/bootstrap.bundle.min.js" 
-    	integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-    	crossorigin="anonymous"
+      src="https://getbootstrap.com/docs/5.1/dist/js/bootstrap.bundle.min.js" 
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+      crossorigin="anonymous"
     ></script>
